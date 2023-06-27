49b5f39028f934e27091ebbebe915b0a6985af19
S T KHOZA <sifiso.khoza@itq.co.za>
2023-06-27 20:22:47 +0200

    files

---
+++ CodeWrinklesSocial/CWK.Domain/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWKSocial.Domain.Models
+{
+    public class Post
+    {
+        public int Id { set; get; }
+        public string Text { set; get; } = null!;
+    }
+}
---
+++ CodeWrinklesSocial/CodeWrinklesSocial.Api/Controllers/PostsController.cs
@@ -0,0 +1,17 @@
+﻿using CWKSocial.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeWrinklesSocial.Api.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PostsController : Controller
+    {    
+        [HttpGet]
+        [Route("{id}")]
+        public  IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id , Text="Hello world"};
+            return Ok(post);
+        }
+    }
